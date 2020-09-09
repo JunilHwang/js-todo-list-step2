a60a61a4a83fda92ce84b8d5c8d7b9caf86f1fe8
junilHwang <tjsdlf4261@naver.com>
2020-09-09 23:03:07 +0900

    refactor: Component를 WebComponent 형태로 리팩토링 중

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,24 +1,51 @@
-import { debounceOneFrame } from "../utils/index.js";
+const Component = (name, propsKeys, render) => {
 
-export const Component = class {
+  const CustomElement = class extends HTMLElement {
 
-  $state = {}; $props; $render;
+    $props;
 
-  constructor(target, state, props) {
-    this.$props = props;
-    this.$render = debounceOneFrame(() => {
-      target.innerHTML = this.render();
-    });
-    this.setEvent(target);
-    this.setState(state);
+    constructor() {
+      super();
+
+      for (const key of propsKeys) {
+        Object.defineProperty(this, key, {
+          set: value => {
+            this.setAttribute(key, value);
+          }
+        })
+      }
+
+      this.$props = propsKeys.reduce((obj, key) => {
+        Object.defineProperty(obj, key, {
+          get: () => { return this.getAttribute(key); },
+          set: value => { this.setAttribute(key, value); }
+        })
+        return obj;
+      }, {});
+
+      this.innerHTML = render(this.$props);
+    }
+
+    static get observedAttributes() {
+      return propsKeys;
+    }
+
+    attributeChangedCallback() {
+      this.innerHTML = render(this.$props);
+    }
   }
 
-  setEvent (target) {}
-  render () { return '' }
+  customElements.define(name, CustomElement);
 
-  setState (payload) {
-    this.$state = { ...this.$state, ...payload };
-    this.$render();
-  }
-
+  return CustomElement;
 }
+
+Component('todo-item',['contents', 'key'], ({ key, contents }) => `
+  <p data-key="${key}">${contents}</p>
+`);
+
+document.querySelector('#app').innerHTML = `
+  <todo-item key="1" contents="test" />
+`
+
+const todoItem = document.querySelector('todo-item');
